72c148920aeffc82517d8090bc4a836e9fc42a6c
Arch1e710 <archielaing@hotmail.com>
2026-07-10 17:36:22 +0100

    Improve comments and fix spelling in simulation code
    Updated comments for clarity and corrected spelling errors.

--- monte-carlo-stock-simualtion.py
+++ monte-carlo-stock-simualtion.py
@@ -10,15 +10,15 @@ daily_volatility = 0.02  # daily volatility (standard deviation)
 
 all_paths = []
 
-for daily_price in range(simulations):
+for daily_price in range(simulations): #Simulates stock price paths 50 times over 252 trading days using Monte Carlo
     day_price = [start_price]
-    while len(day_price) < 252:
-        change = day_price[-1] * (1 + daily_return + daily_volatility * np.random.normal())
+    while len(day_price) < 252: #Each day's price is detirmined by the previous price scaled by random return
+        change = day_price[-1] * (1 + daily_return + daily_volatility * np.random.normal()) #Simplified Geometric Brown Motion (GBM) calculating stock prices
         day_price.append(change)
     all_paths.append(day_price)
 
 
-for path in all_paths:
+for path in all_paths: #plotting all of the different price paths
     plt.plot(path, c = "blue", alpha = 0.3)
 plt.title("Annual price paths")
 plt.xlabel("days")
@@ -26,13 +26,15 @@ plt.ylabel("price")
 plt.axhline(y = 110, c = "red", linestyle = "--")
 plt.show()
 
+
+#Calculating the probability of the stock finishing abover 110 (10% more than the original price)
 gain = []
 for path in all_paths:
     if path[-1] > 110:
         gain.append(path[-1])
 
 prob = len(gain)*100/simulations
-print(prob)
+print("Probability of finishing above a price of 110: " + str(prob) + "%")
 
   
 
